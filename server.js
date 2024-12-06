5b049bfa6ed2bde9275579deffbac16693903a71
Moiz-Maskan-Tech <moiz.rehman@maskantech.com>
2024-12-06 10:38:52 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello invotech");
+});
+
+app.get('/instagram',(req,res) => {
+    res.send("instagram")
+})
+
+app.get('/login',(req,res) => {
+    res.send("login")
+})
+
+app.get('/register',(req,res) => {
+    res.send("register")
+})
+
+//          port
+
+app.listen(port, () => {
+  console.log(`app is listing on port number ${port} `);
+});
+
+// nodejs express mongoose
+
+// 1. Data
+// 2. file
+// 3. third party (api) google,aws,azure,cloudinary,sms,email,payment gateway
+
+//  index.js or server.js
